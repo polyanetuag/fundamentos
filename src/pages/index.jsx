98ef64d199d476d66657c623d6ae5a98333baacf
Polyane Tuag <pollyportugues@hotmail.com>
2021-09-11 17:51:25 -0300

    understanding the jsx structure

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,3 +1,8 @@
 export default function Initial() {
-  return "Initial"
+  return (
+    <div>
+      <h1>Fundamentos do Next.js e React</h1>
+      <h2>Vamos estudar esse framework</h2>
+    </div>
+  )
 }
