9dd97807a772dc3e8fc0f2fa71dd26e47acc5685
Polyane Tuag <pollyportugues@hotmail.com>
2021-09-11 18:37:44 -0300

    modularized CSS

---
+++ src/pages/modular.jsx
@@ -0,0 +1,9 @@
+import styles from '../styles/Modular.module.css'
+
+export default function Modular() {
+  return (
+    <div className={styles.purple}>
+      <h1>Aplicando estilo com css module </h1>
+    </div>
+  )
+}
