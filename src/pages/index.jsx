ae4da21ab2006e68feae25a3c03475a30af948f9
Polyane Tuag <pollyportugues@hotmail.com>
2021-09-12 17:18:49 -0300

    navigation between components

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,8 +1,11 @@
+import Link from 'next/link'
+
 export default function Initial() {
   return (
     <div>
-      <h1>Fundamentos do Next.js e React</h1>
-      <h2>Vamos estudar esse framework</h2>
+      <Link href="/modular" >
+        Aplicando Estilo
+      </Link>
     </div>
   )
 }
--- src/pages/modular.jsx
+++ src/pages/modular.jsx
@@ -1,8 +1,10 @@
 import styles from '../styles/Modular.module.css'
+import Link from 'next/link'
 
 export default function Modular() {
   return (
     <div className={styles.purple}>
+      <Link href="/">Voltar</Link>
       <h1>Aplicando estilo com css module </h1>
     </div>
   )
