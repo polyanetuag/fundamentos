423f017f29c192828c823f6e13982dc0b0cd3427
Polyane Tuag <pollyportugues@hotmail.com>
2021-09-13 14:20:06 -0300

    referencing navigation component

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,11 +1,17 @@
-import Link from 'next/link'
+import Navigation from '../components/Navigation'
 
 export default function Initial() {
   return (
-    <div>
-      <Link href="/modular" >
-        Aplicando Estilo
-      </Link>
+    <div style={{
+      display: 'flex',
+      justifyContent:'center',
+      alignItems:'center',
+      height: '100vh',
+      flexWrap: 'wrap'
+    }}>
+      <Navigation text="Modular" destiny="/modular"/>
+      <Navigation text="Example" destiny="/example" cor="#1bad2a"/>
+      <Navigation text="JSX" destiny="/jsx" cor="crimson"/>
     </div>
   )
 }
