2ad884a2f6abf5eb2b490068bc864d122f068f10
Polyane Tuag <pollyportugues@hotmail.com>
2021-09-13 14:27:58 -0300

    simple navigation

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -12,6 +12,7 @@ export default function Initial() {
       <Navigation text="Modular" destiny="/modular"/>
       <Navigation text="Example" destiny="/example" cor="#1bad2a"/>
       <Navigation text="JSX" destiny="/jsx" cor="crimson"/>
+      <Navigation text="Navegação #01" destiny="/navigation" cor="blue"/>
     </div>
   )
 }
---
+++ src/pages/navigation/index.jsx
@@ -0,0 +1,9 @@
+import Layout from '../../components/Layout'
+
+export default function Navigation() {
+  return (
+    <Layout titulo="Exemplo de Navegação Simples">
+      <h1>Navegação #01</h1>
+    </Layout>
+  )
+}
