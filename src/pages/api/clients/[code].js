cfd0744236705446a0f8427f88ede224b14bc4cd
Polyane Tuag <pollyportugues@hotmail.com>
2021-09-14 16:46:58 -0300

    using api #02

---
+++ src/pages/api/clients/[code].js
@@ -0,0 +1,9 @@
+export default function handler(req, res) {
+  const code = req.query.code;
+
+  res.status(200).json({
+    id: code,
+    name: `Maria ${code}`,
+    email: `maria${code}@xmail.com`,
+  });
+}
